245dfeb33cbd5c9ee0fecc677c5a10524ba00eb8
l0uise <louisepop14@gmail.com>
2024-08-19 18:45:46 +0200

    étape 4: développer les algorithmes de recherche : boucles natives

--- js/index.js
+++ js/index.js
@@ -1,4 +1,63 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const searchInput = document.querySelector('#search-input');
+        
+    //event listenner sur l'input #search-input
+    searchInput.addEventListener('input', function() {
+        const inputRecipe = searchInput.value;
+        const filteredRecipes = rechercher_recette_boucles_natives(recipes, inputRecipe);
+        displayRecipes(filteredRecipes);
+    });
+
+    function rechercher_recette_boucles_natives(recipes, inputRecipe) {
+        //déclaration variables
+        let lowerInputRecipe;
+        let sortedRecipes = new Set();
+        let titreMatch, ingredientMatch, descriptionMatch;
+
+        const alert = document.querySelector('.alert');
+        
+        //vérification taille recette
+        if (inputRecipe.length >= 3) {
+            //masque l'alerte si 4 caractères ou plus
+            alert.classList.add('fade');
+            alert.classList.remove('show');
+
+            //initialisation des variables
+            lowerInputRecipe = inputRecipe.toLowerCase();
+        
+            //parcours chaque recette
+            for (let recipe of recipes) {
+                titreMatch = recipe.name.toLowerCase().includes(lowerInputRecipe);
+                ingredientMatch = false;
+                descriptionMatch = recipe.description.toLowerCase().includes(lowerInputRecipe);
+        
+                //parcours chaque ingrédient
+                for (let ingredient of recipe.ingredients) {
+                    if (ingredient.ingredient.toLowerCase().includes(lowerInputRecipe)) {
+                        ingredientMatch = true;
+                        break; //sort de la boucle si un ingrédient correspond
+                    }
+                }
+        
+                //ajt recette au set si une correspondance est trouvée
+                if (titreMatch || ingredientMatch || descriptionMatch) {
+                    sortedRecipes.add(recipe);
+                }
+            }
+            //met à jours le nombre de recette
+            updateTotalRecipes(Array.from(sortedRecipes));
+            //return la liste des recettes filtrées
+            return Array.from(sortedRecipes);
+            
+        } else {
+            updateTotalRecipes(Array.from(sortedRecipes));
+            //affiche l'alerte si moins de 3 caractères
+            alert.classList.add('show');
+            alert.classList.remove('fade');
+        }
+    }
+
     //total des recettes
     function updateTotalRecipes(recettes){
         const blocTotalRecettes = document.querySelector('.bloc-total-recettes');
@@ -10,7 +69,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     displayRecipes(recipes);
     updateTotalRecipes(recipes);
-
-    
-    
 });
