7607db80fe60db5d713b20f1f1b80e9a98a633f0
l0uise <louisepop14@gmail.com>
2024-08-20 17:41:04 +0200

    étape 1: implémenter l'interface #1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const searchInput = document.querySelector('#search-input');
+    const clearIcon = document.querySelector('.clear-icon-header');
+
+    //event listenner sur l'input #search-input
+    searchInput.addEventListener('input', function() {
+        clearIcon.style.display = 'block';
+    });
+
+    clearIcon.addEventListener('click', function() {
+        searchInput.value = '';
+        clearIcon.style.display = 'none';
+        searchInput.focus();
+    });
+
     //total des recettes
     function updateTotalRecipes(recettes){
         const blocTotalRecettes = document.querySelector('.bloc-total-recettes');
@@ -11,6 +26,4 @@ document.addEventListener('DOMContentLoaded', () => {
     displayRecipes(recipes);
     updateTotalRecipes(recipes);
 
-    
-    
 });
