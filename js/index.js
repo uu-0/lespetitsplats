ca174109bb093314f16f0ef3e162c522cefab7c9
l0uise <louisepop14@gmail.com>
2024-08-29 16:21:53 +0200

    étape 5: choisir le meilleur algorithme en testant leur performance #5

--- js/index.js
+++ js/index.js
@@ -2,24 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const searchInput = document.querySelector('#search-input');
     const clearIcon = document.querySelector('.clear-icon-header');
+    const alert = document.querySelector('.alert');
         
     //event listenner sur l'input #search-input
     searchInput.addEventListener('input', function() {
+        //recette cherchée par l'user
         const inputRecipe = searchInput.value;
-        const filteredRecipes = rechercher_recette_programmation_fonctionnelle(recipes, inputRecipe);
-        displayRecipes(filteredRecipes);
-        clearIcon.style.display = 'block';
+
+        //si l'input est vide, affiche les recettes non filtrées
+        //trim() permet de supprimer les espaces blancs, donc même si l'user entre des espaces on affiche les recettes nn filtrées
+        if (inputRecipe.trim() === '') {
+            //cache l'icône de clear si l'input est vide
+            clearIcon.style.display = 'none'; 
+        }else if (inputRecipe.length <3){
+            displayRecipes(recipes);
+            updateTotalRecipes(recipes);
+            //affiche l'alerte si moins de 3 caractères
+            alert.classList.add('show');
+            alert.classList.remove('fade');
+            clearIcon.style.display = 'block';
+        }else{
+            const filteredRecipes = rechercher_recette_programmation_fonctionnelle(recipes, inputRecipe);
+            displayRecipes(filteredRecipes);
+            clearIcon.style.display = 'block'; // Afficher l'icône de clear s'il y a du texte
+        }
     });
 
     clearIcon.addEventListener('click', function() {
+        //affiche les recettes non filtrées + total quand on vide l'input
+        displayRecipes(recipes); 
+        updateTotalRecipes(recipes);
+        //vide l'input
         searchInput.value = '';
         clearIcon.style.display = 'none';
         searchInput.focus();
     });
 
     function rechercher_recette_programmation_fonctionnelle(recipes, inputRecipe) {
-        const alert = document.querySelector('.alert');
-
         //vérifie la longueur de la recette recherchée
         if (inputRecipe.length >= 3) {
             //masque l'alerte si 4 caractères ou plus
@@ -32,11 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
             //filtre les recettes
             const filtredRecipes = recipes.filter(recipe => {
                 const titreMatch = recipe.name.toLowerCase().includes(lowerInputRecipe);
-                console.log(titreMatch);
                 const ingredientMatch = recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(lowerInputRecipe));
-                console.log(ingredientMatch);
                 const descriptionMatch = recipe.description.toLowerCase().includes(lowerInputRecipe);
-                console.log(descriptionMatch);
+
                 return titreMatch || ingredientMatch || descriptionMatch;
             });
         
@@ -59,7 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateTotalRecipes(recettes){
         const blocTotalRecettes = document.querySelector('.bloc-total-recettes');
         const nombreRecettes = recettes.length;
-
+        //on vide le contenu actuel et on ajoute le nouveau total
         blocTotalRecettes.innerHTML = " ";
         blocTotalRecettes.innerHTML = nombreRecettes + " recettes";
     }
